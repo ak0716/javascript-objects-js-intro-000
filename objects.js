06659c4d37cc13055f116e0b1d291724e1b26f24
Ariel Reuven Kogan <ar.kogan@gmail.com>
2020-09-14 00:45:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,9 +4,7 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  //return Object.assign({}, playlist, {artistName: songTitle})
-  playlist.artistName = songTitle
-  return playlist
+  return Object.assign({}, playlist, {[artistName]: songTitle})
 }
 
 updatePlaylist(playlist, 'Phil Ochs', 'Here\'s to the State of Mississippi')
